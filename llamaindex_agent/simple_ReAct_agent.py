455f3ed061cf208bfff1701d397dc94b16de2935
Ferda <fsuarezj@gmail.com>
2024-02-17 17:48:17 +0000

    Migrated to llama-index 0.10.X

--- llamaindex_agent/simple_ReAct_agent.py
+++ llamaindex_agent/simple_ReAct_agent.py
@@ -1,6 +1,7 @@
-from llama_index.agent import ReActAgent
-from llama_index.llms import ChatMessage, OpenAI
-from llama_index.tools import BaseTool, FunctionTool
+from llama_index.core.agent import ReActAgent
+from llama_index.core.llms import ChatMessage
+from llama_index.llms.openai import OpenAI
+from llama_index.core.tools import BaseTool, FunctionTool
 from global_conf import GPT_MODEL
 
 def multiply(a: int, b: int) -> int:
@@ -17,7 +18,8 @@ add_tool = FunctionTool.from_defaults(fn=add)
 llm = OpenAI(model=GPT_MODEL)
 agent = ReActAgent.from_tools([multiply_tool, add_tool], llm=llm, verbose=True)
 
-#response_gen = agent.chat("What is 20+(2*4)? Calculate step by step")
+response_gen = agent.chat("What is 20+(2*4)? Calculate step by step")
 prompt_dict = agent.get_prompts()
+print(prompt_dict)
 for k, v in prompt_dict.items():
     print(f"Prompt: {k}\n\nValue: {v.template}")
